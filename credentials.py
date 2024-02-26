80426b33673745dad44ce117ec9d87dcc45ec2de
Macha-Akhil <akhilmacha996646@gmail.com>
2024-02-26 10:45:51 +0530

    creds Updated

--- credentials.py
+++ credentials.py
@@ -1,7 +1,18 @@
 username = 'REUG1397'
 pwd = '2209'
-api_key = 'IiO0DeTk'
+api_key = 'CcAPpUm8'
 token = "3ENHXY6BSSGFFWGFOEY2GPGQPM"
 feed_token = None
 token_map = None
-secret_key = '9601574b-c305-4824-b2ef-016e138e6aef'
+secret_key = '3b7a1abd-071d-40d1-a72a-3cf1814be926'
+
+# ----------------------------------------------------------
+# LocalHost 
+
+# username = 'REUG1397'
+# pwd = '2209'
+# api_key = 'IiO0DeTk'
+# token = "3ENHXY6BSSGFFWGFOEY2GPGQPM"
+# feed_token = None
+# token_map = None
+# secret_key = '9601574b-c305-4824-b2ef-016e138e6aef'
