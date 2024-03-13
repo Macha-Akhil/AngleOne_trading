8dd026768f90348e0871c3e03975a13dbbb674d4
Macha-Akhil <akhilmacha996646@gmail.com>
2024-03-13 10:19:42 +0530

    version 5.0

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from pytz import timezone
 from operations import get_index_info,get_strike_lowprice,buy_stock,check_and_cancel_order,orderlist_check_placesell
 # Create a Flask web application
 app = Flask(__name__,template_folder='templates',static_folder='static')
-app.config['SERVER_TIMEOUT'] = 1200
+app.config['SERVER_TIMEOUT'] = 12000
 #os.environ['TZ'] = 'Asia/Kolkata'
 # Angleone Client Credentials
 app.secret_key =credentials.secret_key
@@ -59,7 +59,7 @@ def main():
             return json.dumps({"Error in post values :":str(e)}),500
         dynamic_time_int = int(dynamic_time)
         get_index = get_index_info(dynamic_time_int,dynamic_index,smartapi)
-        return str(get_index)
+        #return str(get_index)
         if not isinstance(get_index, (int, float)):
         # If it's not a float or int, raise a ValueError
             raise ValueError("Invalid index value: {}".format(get_index))
